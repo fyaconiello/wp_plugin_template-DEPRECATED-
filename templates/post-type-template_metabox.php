1240c17a824c79be6cda782ee537f64fbd854afe
Francis Yaconiello <fyaconiello@users.noreply.github.com>
2014-02-20 09:57:18 -0500

    closing tr tags properly

--- templates/post-type-template_metabox.php
+++ templates/post-type-template_metabox.php
@@ -6,7 +6,7 @@
         <td>
             <input type="text" id="meta_a" name="meta_a" value="<?php echo @get_post_meta($post->ID, 'meta_a', true); ?>" />
         </td>
-    <tr>
+    </tr>
     <tr valign="top">
         <th class="metabox_label_column">
             <label for="meta_a">Meta B</label>
@@ -14,7 +14,7 @@
         <td>
             <input type="text" id="meta_b" name="meta_b" value="<?php echo @get_post_meta($post->ID, 'meta_b', true); ?>" />
         </td>
-    <tr>
+    </tr>
     <tr valign="top">
         <th class="metabox_label_column">
             <label for="meta_a">Meta C</label>
@@ -22,5 +22,5 @@
         <td>
             <input type="text" id="meta_c" name="meta_c" value="<?php echo @get_post_meta($post->ID, 'meta_c', true); ?>" />
         </td>
-    <tr>                
+    </tr>                
 </table>
